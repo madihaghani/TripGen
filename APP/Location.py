be7eee0cfd824493c48b5594bca57691b77ce548
Madiha Ghani <81394295+madihaghani@users.noreply.github.com>
2021-03-27 13:56:53 -0400

    Create Location.py

---
+++ APP/Location.py
@@ -0,0 +1,15 @@
+import requests
+
+class Location:
+    def __init__(self, query):
+        self.query = query
+
+    @staticmethod
+    def set_attributes(query):
+        pass
+
+
+
+
+if __name__ == '__main__':
+    loc = Location('university of chicago')
