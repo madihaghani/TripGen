d5e69925563c6792b457d53f7a81627517275505
Madiha Ghani <81394295+madihaghani@users.noreply.github.com>
2021-03-27 15:30:36 -0400

    Update Location.py

--- APP/Location.py
+++ APP/Location.py
@@ -1,15 +1,35 @@
 import requests
+import constants
+import os
+import json
 
 class Location:
     def __init__(self, query):
         self.query = query
+        self.latitude, self.longitude, self.address = self.set_attributes(self.query)
 
     @staticmethod
     def set_attributes(query):
-        pass
+        #https://api.tomtom.com/search/2/geocode/Saginaw%20MI.json?&key=*****
+        _PATH = os.path.join(constants.TOMTOM_API_URL, query + constants.EXTENSION + constants.TOMTOM_API_KEY)
+        _response = requests.get(_PATH)
+        _json_data = json.loads(_response.text)
+        print(_json_data)
+        return _json_data['results'][0]['position']['lat'], \
+               _json_data['results'][0]['position']['lon'], \
+               _json_data['results'][0]['address']['freeformAddress']
+
+    def __str__(self):
+        return self.address
+
+
 
 
 
 
 if __name__ == '__main__':
     loc = Location('university of chicago')
+    print(loc.latitude)
+    print(loc.longitude)
+    print(loc.address)
+    print(loc)
